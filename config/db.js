831695cf3e2076e4e15933950f8d8dbe646098ae
Godswill Umukoro <umukoro6@gmail.com>
2022-09-15 13:52:14 +0100

    mongodb deploy issues

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGODB_URI)
+    const conn = await mongoose.connect(process.env.MONGODB_URI, err => {
+        if (err) {
+            console.log(err)
+        }
+    })
 
     console.log(`MongoDB Conected: ${conn.connection.host}`)
 }
