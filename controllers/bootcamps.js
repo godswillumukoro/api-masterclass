ca5db4dbfaaad9fbf9a3e159ad5f26598dd5ea83
Godswill Umukoro <umukoro6@gmail.com>
2022-09-28 17:37:54 +0100

    added pagination feature

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -17,7 +17,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = Bootcamp.find(JSON.parse(queryString))
 
     // fields to exclude
-    const removeFields = ['select', 'sort']
+    const removeFields = ['select', 'sort', 'page', 'limit']
     removeFields.forEach(field => delete requestQuery[field])
 
     // Select fields
@@ -35,10 +35,35 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
         query = query.sort('-createdAt')
     }
 
-    // executing resource
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 8
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Bootcamp.countDocuments()
+
+    query = query.skip(startIndex).limit(limit)
+
+    // executing query
     const bootcamps = await query
 
-    res.status(200).json({ success: true, total: bootcamps.length, data: bootcamps })
+    // pagination result
+    const pagination = {}
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit
+        }
+    }
+
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit
+        }
+    }
+
+    res.status(200).json({ success: true, total: bootcamps.length, pagination, data: bootcamps })
 
 })
 
