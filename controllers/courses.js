3fba5a90e9a9f994011e8fd1dae8be06f811c5e1
Godswill Umukoro <umukoro6@gmail.com>
2022-10-01 21:51:29 +0100

    Update a course functionality

--- controllers/courses.js
+++ controllers/courses.js
@@ -65,3 +65,25 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
         data: course
     })
 })
+
+// @desc    Update course
+// @route   PUT /api/v1/courses/:id
+// @access  private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+
+    let course = await Course.findById(req.params.id)
+
+    if (!course) {
+        return next(new ErrorResponse(`No course found with id:${req.params.id}`), 404)
+    }
+
+    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(200).json({
+        success: true,
+        data: course
+    })
+})
